cddbf6396cca39d02e4a9852cd4ec807960f6412
Kyle <kyle@example.com>
2024-12-07 01:32:37 -0500

    compose

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 import Turn from "node-turn";
 
+
+class ZetaRequest {
+  constructor(hash, fragIndex, totalFrags, data) {
+    this.hash = hash;
+    this.fragIndex = fragIndex;
+    this.completedFrags = 1;
+    this.totalFrags = totalFrags;
+    this.data = []
+    this.data[fragIndex] = data;
+  }
+
+  addData(fragIndex, data) {
+    this.data[fragIndex] = data;
+    this.completedFrags++;
+    return this.completedFrags >= this.totalFrags;
+  }
+}
+
+const requests = {}
+
 const listenToPort = 47777;
 const turnServer = new Turn({
   listeningPort: listenToPort,
@@ -8,8 +28,21 @@ const turnServer = new Turn({
     username: "password",
   },
 });
-turnServer.onSdpPacket = async (contents) => {
-  console.log("sdp", JSON.stringify(contents));
+turnServer.onSdpPacket = async (message) => {
+  console.log("sdp", JSON.stringify(message));
+
+  if (message.startsWith("zeta ")) {
+    const [protocol, hash, fragIndex, totalFrags, ...dataParts] = message.split(" ");
+    const data = dataParts.join(" ");
+    if (requests[hash]) {
+      if (requests[hash].addData(fragIndex, data)) {
+        console.log("all data received", requests[hash].data.join(""));
+        delete requests[hash];
+      }
+    } else {
+      requests[hash] = new ZetaRequest(hash, fragIndex, parseInt(totalFrags), data);
+    }
+  }
 }
 turnServer.start();
 
